96710f6a2214c3940541b5f4e0cf2ae34d439083
arif <arif@bitmascot.com>
2021-04-01 22:21:51 +1100

    base

--- cnn_mel.py
+++ cnn_mel.py
@@ -1,9 +1,12 @@
 import savee
 import cnn
+import preprocessor
 
 savee.prepare_if_needed()
 train_x,train_y = savee.get_mel_data("raw")
+train_x = preprocessor.preprocess_mel(train_x)
 test_x,test_y = savee.get_mel_data("test")
+test_x = preprocessor.preprocess_mel(test_x)
 nn = cnn.CNN(train_x, train_y, test_x, test_y)
 nn.train()
 accuracy = nn.test()
