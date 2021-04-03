9822c8c0d6b067f12662a5c7f95f3744b46a2631
arif <arif@bitmascot.com>
2021-04-03 20:54:13 +1100

    done

--- output_layer.py
+++ output_layer.py
@@ -19,8 +19,6 @@ class OutputLayer(Layer):
 
     def backward ( self , dvalues, y_true=None ):
         samples = len (dvalues)
-        if len (y_true.shape) == 2 :
-            y_true = np.argmax(y_true, axis = 1 )
         self.dinputs = dvalues.copy()
         self.dinputs[ range (samples), y_true] -= 1
         self.dinputs = self.dinputs / samples
